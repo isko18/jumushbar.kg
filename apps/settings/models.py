46ba1499e6110242ccfa51a377de2933863263ec
Beksultan06 <nurlanuuulubeksultan@gmail.com>
2025-08-03 15:30:33 +0600

    added apps settings

--- apps/settings/models.py
+++ apps/settings/models.py
@@ -2,29 +2,48 @@ from django.db import models
 from ckeditor.fields import RichTextField
 
 class PrivacyPolicy(models.Model):
-    content = RichTextField(verbose_name="HTML-содержимое")
-    updated_at = models.DateTimeField(auto_now=True)
+    content = RichTextField(verbose_name="HTML-содержимое политики конфиденциальности")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    class Meta:
+        verbose_name = "Политика конфиденциальности"
+        verbose_name_plural = "Политики конфиденциальности"
 
     def __str__(self):
         return "Политика конфиденциальности"
 
+
 class PublicOffer(models.Model):
-    content = RichTextField(verbose_name="HTML-содержимое")
-    updated_at = models.DateTimeField(auto_now=True)
+    content = RichTextField(verbose_name="HTML-содержимое публичной оферты")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    class Meta:
+        verbose_name = "Публичная оферта"
+        verbose_name_plural = "Публичные оферты"
 
     def __str__(self):
         return "Публичная оферта"
 
+
 class RefundPolicy(models.Model):
-    content = RichTextField(verbose_name="HTML-содержимое")
-    updated_at = models.DateTimeField(auto_now=True)
+    content = RichTextField(verbose_name="HTML-содержимое политики возврата")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    class Meta:
+        verbose_name = "Политика возврата"
+        verbose_name_plural = "Политики возврата"
 
     def __str__(self):
         return "Политика возврата"
 
+
 class CardPaymentTerms(models.Model):
-    content = RichTextField(verbose_name="HTML-содержимое")
-    updated_at = models.DateTimeField(auto_now=True)
+    content = RichTextField(verbose_name="HTML-содержимое условий оплаты картой")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    class Meta:
+        verbose_name = "Условия оплаты картой"
+        verbose_name_plural = "Условия оплаты картой"
 
     def __str__(self):
-        return "Оплата банковской картой"
+        return "Условия оплаты картой"
--- core/settings.py
+++ core/settings.py
@@ -245,3 +245,15 @@ JAZZMIN_SETTINGS = {
     },
     "actions_sticky_top": False
 }
+
+# ckeditor
+CKEDITOR_UPLOAD_PATH = "uploads/"
+CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'  # URL to jQuery
+CKEDITOR_IMAGE_BACKEND = "pillow"  # Путь к пакету Pillow для обработки изображений
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',  # Вы можете настроить свою собственную панель инструментов CKEditor
+        'height': 300,
+        'width': 800,
+    },
+}
