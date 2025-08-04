78877a7a3e045ef2061d00a43d530e06f05a5180
Beksultan06 <nurlanuuulubeksultan@gmail.com>
2025-08-04 16:51:30 +0600

    update balance users

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet, OrderListAPI, OrderRespondViewSet, CategoriesListAPI, ReviewViewSet
+from .views import OrderViewSet, OrderListAPI, OrderRespondViewSet, CategoriesListAPI, ReviewViewSet, BalanceUpdateView
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'orders', OrderViewSet, basename='order')
@@ -7,5 +8,10 @@ router.register(r'list-order', OrderListAPI, basename='list-order')
 router.register(r'order-respond', OrderRespondViewSet, basename='order-respond')
 router.register(r'categories-list', CategoriesListAPI, basename='list-categories')
 router.register(r'reviews', ReviewViewSet, basename='reviews')
+
+urlpatterns = [
+    path('user/balance/', BalanceUpdateView.as_view(), name='update-balance'),
+]
+
 
 urlpatterns = router.urls
